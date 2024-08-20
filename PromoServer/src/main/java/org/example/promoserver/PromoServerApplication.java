158f1ad80e70f9cc3fb5d0ffc63fa98e6773a82b
TheAlbatros <thealbatrospl@gmail.com>
2024-08-20 14:05:35 +0200

    Created Project

---
+++ PromoServer/src/main/java/org/example/promoserver/PromoServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.promoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoServerApplication.class, args);
+    }
+
+}
---
+++ PromoServer/src/test/java/org/example/promoserver/PromoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.promoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
